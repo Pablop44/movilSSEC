3032eeea05d5c7c96db14a699bd3b8ab9fd3bfc0
Pablo Pazos Dominguez <ppazos@aldaba.es>
2020-03-11 16:12:14 +0100

    first commit

---
+++ app/src/main/java/com/example/ssec/vistaPrincipalActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vistaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_principal);
+    }
+}
